f669178d5d919e2e12d41421130236b083ed8d8b
hoangtienbng <tiendhpd10190@gmail.com>
2025-02-26 20:39:08 +0700

    Lab8

--- cro101Lab345678/Lab345678/App.js
+++ cro101Lab345678/Lab345678/App.js
@@ -1,11 +1,29 @@
-import React from "react";
-import { StatusBar } from "react-native";
-//mport Lab7 from "./Lab7/bai1.js";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import UserListScreen from './Lab83/ListUser';
+import EditUserScreen from './Lab83/UpdateUser';
+import UserDetailScreen from './Lab83/UserDetailScreen';
+import CreateUserScreen from './Lab83/CreateUser';
+const Stack = createNativeStackNavigator();
 
-import Lab7 from "./Lab7/index.js";
 const App = () => {
-  return <Lab7>
-  </Lab7>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="UserList">
+        <Stack.Screen name="UserList" component={UserListScreen} options={{ title: 'Danh sách người dùng' }} />
+        <Stack.Screen name="EditUser" component={EditUserScreen} options={{ title: 'Chỉnh sửa người dùng' }} />
+        <Stack.Screen name="CreateUser" component={CreateUserScreen} options={{ title: 'Tạo người dùng' }} />
+        <Stack.Screen
+          name="UserDetail"
+          component={UserDetailScreen}
+          options={{ title: 'Chi tiết người dùng' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
+
+
